a847c4547203b64144f7bc1f14ebc7637b41c798
Brandon M. Rodriguez (RIT Student) <bmr1334@ad.rit.edu>
2016-03-09 11:58:32 -0500

    Added in basic game state code, loaded in all textures, started work on collisions, allowed map editor to work on a different screen size and modified game window. Textures are not functioning properly yet (due to the draw statements being used).

--- TheImpossiblerGame/TheImpossiblerGame/Game1.cs
+++ TheImpossiblerGame/TheImpossiblerGame/Game1.cs
@@ -15,15 +15,25 @@ namespace TheImpossiblerGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
         //variables for textures and creating a mapeditor object
-        Texture2D player;
-        Texture2D box;
-        Texture2D triangle;
-        Player p1;
-        Texture2D flip;
+        Texture2D player, box, triangle, flip;
         MapEditor mapEditor;
+        Player p1;
         Box Box;
         Triangle Triangle;
+
+        //texture variables
+        Texture2D logo, spaceBar, options, play, resume, exit, pause, //menus
+            spikeDark, spikeLight, sqCity1, sqCity2, sqLab1, sqLab2, //objects
+            sqSub1, sqSub2, sqSwitchOff, sqSwitchOn, sqWarn, triCity1,
+            triCity2, triLab1, triLab2, triSub1, triSub2, triSwitch;
+
+        //enum to switch game states
+        enum GameState { titleMenu, mainMenu, pauseMenu, game, gameOver, credits };
+        GameState gamestate;
+        KeyboardState kstate, prevKstate;
+        
         //List<Rectangle> boxes;
         //Rectangle platform;
 
@@ -31,7 +41,6 @@ namespace TheImpossiblerGame
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            
         }
 
         /// <summary>
@@ -47,6 +56,7 @@ namespace TheImpossiblerGame
             Box = new Box();
             Triangle = new Triangle();
             p1 = new Player(new Rectangle(0, 0, 30, 45), mapEditor.player);
+            this.IsMouseVisible = true;
             base.Initialize();
             ChangeWindowsSize(); //changes the window size when game initially runs
         }
@@ -66,6 +76,35 @@ namespace TheImpossiblerGame
             triangle = this.Content.Load<Texture2D>("TriangleLab1");
             flip = this.Content.Load<Texture2D>("TriangleLab1Flip");
             player = this.Content.Load<Texture2D>("Player");
+
+            //general menu loads
+            logo = Content.Load<Texture2D>("Menus\\Logo");
+            exit = Content.Load<Texture2D>("Menus\\ExitText");
+            options = Content.Load<Texture2D>("Menus\\OptionsText");
+            pause = Content.Load<Texture2D>("Menus\\Pause");
+            play = Content.Load<Texture2D>("Menus\\PlayText");
+            resume = Content.Load<Texture2D>("Menus\\ResumeText");
+            spaceBar = Content.Load<Texture2D>("Menus\\SpacebarText");
+
+            //general game object loads
+            spikeDark = Content.Load<Texture2D>("Game Textures\\SpikeDark");
+            spikeLight = Content.Load<Texture2D>("Game Textures\\SpikeLight");
+            sqCity1 = Content.Load<Texture2D>("Game Textures\\SquareCity1");
+            sqCity2 = Content.Load<Texture2D>("Game Textures\\SquareCity2");
+            sqLab1 = Content.Load<Texture2D>("Game Textures\\SquareLab1");
+            sqLab2 = Content.Load<Texture2D>("Game Textures\\SquareLab2");
+            sqSub1 = Content.Load<Texture2D>("Game Textures\\SquareSubway1");
+            sqSub2 = Content.Load<Texture2D>("Game Textures\\SquareSubway2");
+            sqSwitchOff = Content.Load<Texture2D>("Game Textures\\SquareSwitchOff");
+            sqSwitchOn = Content.Load<Texture2D>("Game Textures\\SquareSwitchOn");
+            sqWarn = Content.Load<Texture2D>("Game Textures\\SquareWarning");
+            triCity1 = Content.Load<Texture2D>("Game Textures\\TriangleCity1");
+            triCity2 = Content.Load<Texture2D>("Game Textures\\TriangleCity2");
+            triLab1 = Content.Load<Texture2D>("Game Textures\\TriangleLab1");
+            triLab2 = Content.Load<Texture2D>("Game Textures\\TriangleLab2");
+            triSub1 = Content.Load<Texture2D>("Game Textures\\TriangleSubway1");
+            triSub2 = Content.Load<Texture2D>("Game Textures\\TriangleSubway2");
+            triSwitch = Content.Load<Texture2D>("Game Textures\\TriangleSwitch");
         }
 
         /// <summary>
@@ -84,7 +123,7 @@ namespace TheImpossiblerGame
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.None))
                 Exit();
 
             // TODO: Add your update logic here
@@ -97,7 +136,32 @@ namespace TheImpossiblerGame
             mapEditor.LoadTextFile(); //calls method to find the file
             mapEditor.ReadTextFile(); //calls method to read the file
             mapEditor.GeneratePlatforms();
-            
+
+            //switches between states
+            switch (gamestate)
+            {
+                case GameState.titleMenu:
+                    kstate = Keyboard.GetState();
+                    if (kstate.IsKeyDown(Keys.Space) && prevKstate.IsKeyUp(Keys.Space)) gamestate = GameState.mainMenu;
+                    break;
+                case GameState.mainMenu:
+                    //kstate = Keyboard.GetState();
+                    //if (kstate.IsKeyDown(Keys.Space) && prevKstate.IsKeyUp(Keys.Space)) gamestate = GameState.mainMenu;
+                    break;
+                case GameState.credits:
+                    kstate = Keyboard.GetState();
+                    break;
+                case GameState.game:
+                    kstate = Keyboard.GetState();
+                    if (kstate.IsKeyDown(Keys.Escape) && prevKstate.IsKeyUp(Keys.Escape)) gamestate = GameState.pauseMenu;
+                    break;
+                case GameState.gameOver:
+                    kstate = Keyboard.GetState();
+                    break;
+                case GameState.pauseMenu:
+                    kstate = Keyboard.GetState();
+                    break;
+            }
 
             foreach (Rectangle r in mapEditor.Squares)
             {
@@ -116,6 +180,9 @@ namespace TheImpossiblerGame
             }
 
             base.Update(gameTime);
+
+            //gets previos keyboard state
+            prevKstate = kstate;
         }
 
         /// <summary>
@@ -128,12 +195,47 @@ namespace TheImpossiblerGame
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-             mapEditor.Draw(spriteBatch); //draws the objects in the text file
-           // Box.Draw(spriteBatch);
+            mapEditor.Draw(spriteBatch); //draws the objects in the text file
+            // Box.Draw(spriteBatch);
             //Triangle.Draw(spriteBatch);
+
+            //game state switches
+            switch (gamestate)
+            {
+                case GameState.titleMenu:
+                    //Color trans = new Color(255, 255, 255, 0);
+                    spriteBatch.Draw(logo, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 3 + GraphicsDevice.DisplayMode.Width / 25,
+                        GraphicsDevice.DisplayMode.Height / 4 - GraphicsDevice.DisplayMode.Height / 6, 1200, 500), Color.White);
+                    spriteBatch.Draw(spaceBar, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 4 + GraphicsDevice.DisplayMode.Width / 12,
+                        GraphicsDevice.DisplayMode.Height / 2 + GraphicsDevice.DisplayMode.Height / 7, 500, 100), Color.White);
+                    break;
+                case GameState.mainMenu:
+                    spriteBatch.Draw(logo, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 3 + GraphicsDevice.DisplayMode.Width / 25,
+                        GraphicsDevice.DisplayMode.Height / 4 - GraphicsDevice.DisplayMode.Height / 6, 1200, 500), Color.White);
+                    spriteBatch.Draw(play, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 4 + GraphicsDevice.DisplayMode.Width / 12,
+                        GraphicsDevice.DisplayMode.Height / 2 + GraphicsDevice.DisplayMode.Height / 7, 500, 100), Color.White);
+                    spriteBatch.Draw(exit, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 4 + GraphicsDevice.DisplayMode.Width / 12,
+                        GraphicsDevice.DisplayMode.Height / 2 + GraphicsDevice.DisplayMode.Height / 4, 500, 100), Color.White);
+
+                    break;
+                case GameState.credits:
+
+                    break;
+                case GameState.game:
+
+                    break;
+                case GameState.gameOver:
+
+                    break;
+                case GameState.pauseMenu:
+                    spriteBatch.Draw(pause, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 3 + GraphicsDevice.DisplayMode.Width / 7,
+                        GraphicsDevice.DisplayMode.Height / 4 - GraphicsDevice.DisplayMode.Height / 6, 700, 300), Color.White);
+                    spriteBatch.Draw(resume, new Rectangle(GraphicsDevice.DisplayMode.Width / 2 - GraphicsDevice.DisplayMode.Width / 4 + GraphicsDevice.DisplayMode.Width / 12,
+                       GraphicsDevice.DisplayMode.Height / 2, 500, 100), Color.White);
+                    break;
+            }
+
             spriteBatch.End();
-
-
             base.Draw(gameTime);
         }
 
@@ -146,8 +248,5 @@ namespace TheImpossiblerGame
             graphics.IsFullScreen = true;
             graphics.ApplyChanges();
         }
-
-
-
     }
 }
